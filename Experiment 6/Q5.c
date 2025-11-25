9b284d085a2c57e29f84fe5c7094dbcdc38286db
abhinavparthnautiyal <abhinav07nautiyal@gmail.com>
2025-11-25 22:29:36 +0530

    lab assignments

--- Experiment 6/Q5.c
+++ Experiment 6/Q5.c
@@ -1,3 +1,10 @@
 //5.	Develop a function REVERSE (str) that accepts a string argument. Write a C program that invokes this function to find the reverse of a given string. 
 #include<stdio.h>
 void main()
+{
+    char str[100];
+    printf("Enter a string: ");
+    gets(str);
+    reverse(str);
+    printf("Reverse of the string: %s", str);
+}

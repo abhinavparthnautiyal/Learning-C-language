aa3d05fe2bbbe62f7fca976f3295af5763a28e89
abhinavparthnautiyal <abhinav07nautiyal@gmail.com>
2025-11-25 22:53:31 +0530

    Lab Assignments

--- Experiment 6/Q1.c
+++ Experiment 6/Q1.c
@@ -2,18 +2,23 @@
 #include<stdio.h>
 
 int FACT(int n)
-{ 
-    if (n==0 ||n==1)
-    return n;
-    else
+{
+    if(n==0||n==1) return 1;
     return n*FACT(n-1);
+}
+
+int FACT_NONREC(int n)
+{
+    int i,f=1;
+    for(i=1;i<=n;i++) f=f*i;
+    return f;
 }
 
 void main()
 {
-   int num;
-    printf("Enter the number whose factorial you want to find:\n");
-    scanf("%d", &num);
-    int factorial = FACT(num);
-   printf("The factorial of %d is %d", num, factorial);
+    int num;
+    printf("Enter the number:\n");
+    scanf("%d",&num);
+    printf("Recursive factorial of %d is %d\n",num,FACT(num));
+    printf("Non recursive factorial of %d is %d\n",num,FACT_NONREC(num));
 }
